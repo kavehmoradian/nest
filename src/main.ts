5b784d915f86fc446650f62d63820caa4033b9cd
kaveh moradian <kave.moradian@gmail.com>
2023-07-03 15:52:01 +0330

    swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('mySite')
+    .setDescription('The MySite API description')
+    .setVersion('1.0')
+    .addTag('site')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(3001);
 }
 bootstrap();
