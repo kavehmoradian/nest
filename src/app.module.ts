6dc235ee57b13a136ae0fe7ec22072ec7a1edc9b
kaveh moradian <kave.moradian@gmail.com>
2023-07-03 17:50:55 +0330

    add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
 })
 export class AppModule {}
